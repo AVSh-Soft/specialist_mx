ce56b54f9ef3dc707b0ca15786d183abcaefe59c
Alexander Shevtsov <avsh-soft@yandex.ru>
2018-05-21 21:00:39 +0300

    Полное переименование классов и пакетов - НАЧАЛО

---
+++ src/main/java/ru/avsh/specialist/mx/gui/MainFormFX.java
@@ -0,0 +1,7 @@
+package ru.avsh.specialist.mx.gui;
+
+import javafx.scene.layout.Pane;
+
+public class MainFormFX extends Pane {
+    // https://examples.javacodegeeks.com/desktop-java/javafx/javafx-canvas-example/
+}
