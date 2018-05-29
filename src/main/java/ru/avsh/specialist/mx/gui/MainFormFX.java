711fc29a10735064ca668211783a3deb4e6964de
Alexander Shevtsov <avsh-soft@rambler.ru>
2018-05-29 17:39:52 +0300

    Переход на fx (переделка главного окна на JavaFX)

--- src/main/java/ru/avsh/specialist/mx/gui/MainFormFX.java
+++ src/main/java/ru/avsh/specialist/mx/gui/MainFormFX.java
@@ -2,6 +2,9 @@ package ru.avsh.specialist.mx.gui;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
@@ -10,6 +13,7 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 import ru.avsh.specialist.mx.root.SpecialistMX;
+import ru.avsh.specialist.mx.units.memory.sub.ScreenFx;
 
 import static javafx.stage.WindowEvent.WINDOW_CLOSE_REQUEST;
 import static ru.avsh.specialist.mx.helpers.Constants.INI_SECTION_CONFIG;
@@ -21,8 +25,13 @@ import static ru.avsh.specialist.mx.helpers.Constants.SPMX_NAME;
  * @author -=AVSh=-
  */
 public class MainFormFX extends Application {
-    private static final String INI_OPTION_FRAME_WIDTH = "MainFrameWidth";
-    private static final String INI_OPTION_FRAME_HEIGHT = "MainFrameHeight";
+    private static final double IMAGE_WIDTH         = ScreenFx.SCREEN_WIDTH ;
+    private static final double IMAGE_HEIGHT        = ScreenFx.SCREEN_HEIGHT;
+    private static final double IMAGE_DOUBLE_WIDTH  = ScreenFx.SCREEN_WIDTH  << 1;
+    private static final double IMAGE_DOUBLE_HEIGHT = ScreenFx.SCREEN_HEIGHT << 1;
+
+    private static final String INI_OPTION_FORM_WIDTH  = "MainFormWidth" ;
+    private static final String INI_OPTION_FORM_HEIGHT = "MainFormHeight";
     private static final String DISK_A   =   "[A:] - ";
     private static final String DISK_B   =   "[B:] - ";
     private static final String NO_DISK  = "нет диска";
@@ -71,16 +80,57 @@ public class MainFormFX extends Application {
         final MenuBar menuBar = new MenuBar(fileMenu, viewMenu, aboutMenu);
 
         final ImageView imageView = new ImageView(fSpMX.getScreen());
-        imageView.setPreserveRatio(true);
-        imageView.setSmooth(false);
-        imageView.setCache(true);
+        imageView.setPreserveRatio(true );
+        imageView.setSmooth       (false);
+        imageView.setCache        (true );
 
-        final VBox root = new VBox(menuBar, imageView);
-        final Scene scene = new Scene(root, root.getPrefWidth(), root.getPrefHeight());
+        final Button button0 = new Button("Button0");
+        final Button button1 = new Button("Button1");
+        final ButtonBar buttonBar = new ButtonBar();
+        buttonBar.getButtons().addAll(button0, button1);
+        buttonBar.setPadding(new Insets(5.0, 5.0, 5.0, 5.0));
+
+        final VBox  root  = new VBox(menuBar, imageView, buttonBar);
+        final Scene scene = new Scene(root, -1, -1);
         primaryStage.setScene(scene);
-        //primaryStage.setMinWidth(width);
-        //primaryStage.setMinHeight(height);
         primaryStage.show();
+
+
+        primaryStage.setMinWidth (primaryStage.getWidth ());
+        primaryStage.setMinHeight(primaryStage.getHeight());
+
+        final double deltaW = primaryStage.getWidth () - IMAGE_WIDTH ;
+        final double deltaH = primaryStage.getHeight() - IMAGE_HEIGHT;
+
+        imageView.fitWidthProperty().addListener((obs, oldValue, newValue) -> {
+            size11Item.setSelected((double) newValue == IMAGE_WIDTH       );
+            size21Item.setSelected((double) newValue == IMAGE_DOUBLE_WIDTH);
+        });
+
+        imageView.setFitWidth (IMAGE_WIDTH );
+        imageView.setFitHeight(IMAGE_HEIGHT);
+
+        primaryStage.widthProperty ().addListener((obs, oldValue, newValue) -> {
+            imageView.setFitWidth ((Double) newValue - deltaW);
+        });
+        primaryStage.heightProperty().addListener((obs, oldValue, newValue) -> {
+            imageView.setFitHeight((Double) newValue - deltaH);
+        });
+
+        // -= Изменение размеров окна программы =-
+        final EventHandler<ActionEvent> sizeEventHandler = event -> {
+            final CheckMenuItem checkMenuItem = (CheckMenuItem) event.getSource();
+            if (size11Item.equals(checkMenuItem)) {
+                primaryStage.setWidth (deltaW + IMAGE_WIDTH );
+                primaryStage.setHeight(deltaH + IMAGE_HEIGHT);
+            } else {
+                primaryStage.setWidth (deltaW + IMAGE_DOUBLE_WIDTH );
+                primaryStage.setHeight(deltaH + IMAGE_DOUBLE_HEIGHT);
+            }
+            primaryStage.centerOnScreen();
+        };
+        size11Item.setOnAction(sizeEventHandler);
+        size21Item.setOnAction(sizeEventHandler);
 
         exitItem.setOnAction(event -> primaryStage.fireEvent(new WindowEvent(primaryStage, WINDOW_CLOSE_REQUEST)));
 
@@ -107,16 +157,14 @@ public class MainFormFX extends Application {
             }
         });
 
-
-
         // -= Обработка событий клавиатуры (отпускание) =-
         scene.setOnKeyReleased(event -> fSpMX.keyCodeReceiver(false, event.getCode()));
 
         // Обработчик вызывается, когда окно закрывается - для завершения работы программы
         primaryStage.setOnCloseRequest(event -> {
-            // Запоминаем размеры фрейма в ini-файл
-            fSpMX.putIni(INI_SECTION_CONFIG, INI_OPTION_FRAME_WIDTH , primaryStage.getWidth ());
-            fSpMX.putIni(INI_SECTION_CONFIG, INI_OPTION_FRAME_HEIGHT, primaryStage.getHeight());
+            // Запоминаем размеры формы в ini-файле
+            fSpMX.putIni(INI_SECTION_CONFIG, INI_OPTION_FORM_WIDTH, primaryStage.getWidth ());
+            fSpMX.putIni(INI_SECTION_CONFIG, INI_OPTION_FORM_HEIGHT, primaryStage.getHeight());
             // Сохраняем ini-файл
             fSpMX.storeIni();
             // Завершаем приложение JavaFX
