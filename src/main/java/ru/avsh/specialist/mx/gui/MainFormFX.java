a37aff22950730ff8fc50b0cab8b54d4786db593
Alexander Shevtsov <avsh-soft@rambler.ru>
2018-05-30 10:13:21 +0300

    Переход на fx (переделка главного окна на JavaFX)

--- src/main/java/ru/avsh/specialist/mx/gui/MainFormFX.java
+++ src/main/java/ru/avsh/specialist/mx/gui/MainFormFX.java
@@ -15,9 +15,14 @@ import javafx.stage.WindowEvent;
 import ru.avsh.specialist.mx.root.SpecialistMX;
 import ru.avsh.specialist.mx.units.memory.sub.ScreenFx;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Properties;
+
+import static javafx.scene.control.Alert.AlertType.INFORMATION;
 import static javafx.stage.WindowEvent.WINDOW_CLOSE_REQUEST;
-import static ru.avsh.specialist.mx.helpers.Constants.INI_SECTION_CONFIG;
-import static ru.avsh.specialist.mx.helpers.Constants.SPMX_NAME;
+import static ru.avsh.specialist.mx.helpers.Constants.*;
 
 /**
  * Класс для формирования главного окна приложения (меню, кнопоки обработчики событий).
@@ -70,9 +75,15 @@ public class MainFormFX extends Application {
         final RadioMenuItem modeMXItem = new RadioMenuItem("Клавиатура \"".concat(SPMX_NAME).concat("\""));
         final RadioMenuItem modeSTItem = new RadioMenuItem("Клавиатура \"Специалист\"");
         final      Menu       viewMenu = new      Menu    ("Просмотр", null,
-                                           infoItem,  debugItem,
+                infoItem,  debugItem,
                 new SeparatorMenuItem(), size11Item, size21Item,
                 new SeparatorMenuItem(), modeMXItem, modeSTItem);
+
+        final   ToggleGroup       group = new ToggleGroup();
+        modeMXItem.setToggleGroup(group);
+        modeSTItem.setToggleGroup(group);
+        modeMXItem.setSelected(!fSpMX.isKeyboardMode());
+        modeSTItem.setSelected( fSpMX.isKeyboardMode());
 
         final MenuItem aboutItem = new MenuItem("О программе...");
         final Menu     aboutMenu = new Menu    ("О программе", null, aboutItem);
@@ -84,17 +95,24 @@ public class MainFormFX extends Application {
         imageView.setSmooth       (false);
         imageView.setCache        (true );
 
-        final Button button0 = new Button("Button0");
-        final Button button1 = new Button("Button1");
+        final Button      openBtn = new Button("Открыть"  );
+        final Button      saveBtn = new Button("Сохранить");
+        final Button     resetBtn = new Button("Сбросить" );
+        final Button     debugBtn = new Button("Отладчик" );
         final ButtonBar buttonBar = new ButtonBar();
-        buttonBar.getButtons().addAll(button0, button1);
+        buttonBar.getButtons().addAll(openBtn, saveBtn, resetBtn, debugBtn);
         buttonBar.setPadding(new Insets(5.0, 5.0, 5.0, 5.0));
+
+         openBtn.setTooltip(new Tooltip("Открывает (загружает и запускает) файлы *.rom, *.mon, *.cpu(i80), *.rks, *.odi"));
+         saveBtn.setTooltip(new Tooltip("Сохраняет блок данных в файлы *.cpu(i80) и *.rks"));
+        resetBtn.setTooltip(new Tooltip("Сбрасывает эмулятор в исходное состояние"));
+        debugBtn.setTooltip(new Tooltip("Запускает отладчик"));
 
         final VBox  root  = new VBox(menuBar, imageView, buttonBar);
         final Scene scene = new Scene(root, -1, -1);
         primaryStage.setScene(scene);
         primaryStage.show();
-
+        //--------------------------------------------------------------------------------------------------------------
 
         primaryStage.setMinWidth (primaryStage.getWidth ());
         primaryStage.setMinHeight(primaryStage.getHeight());
@@ -110,12 +128,8 @@ public class MainFormFX extends Application {
         imageView.setFitWidth (IMAGE_WIDTH );
         imageView.setFitHeight(IMAGE_HEIGHT);
 
-        primaryStage.widthProperty ().addListener((obs, oldValue, newValue) -> {
-            imageView.setFitWidth ((Double) newValue - deltaW);
-        });
-        primaryStage.heightProperty().addListener((obs, oldValue, newValue) -> {
-            imageView.setFitHeight((Double) newValue - deltaH);
-        });
+        primaryStage.widthProperty ().addListener((obs, oldValue, newValue) -> imageView.setFitWidth ((Double) newValue - deltaW));
+        primaryStage.heightProperty().addListener((obs, oldValue, newValue) -> imageView.setFitHeight((Double) newValue - deltaH));
 
         // -= Изменение размеров окна программы =-
         final EventHandler<ActionEvent> sizeEventHandler = event -> {
@@ -133,6 +147,35 @@ public class MainFormFX extends Application {
         size21Item.setOnAction(sizeEventHandler);
 
         exitItem.setOnAction(event -> primaryStage.fireEvent(new WindowEvent(primaryStage, WINDOW_CLOSE_REQUEST)));
+
+        // -= Режим работы клавиатуры =-
+        modeMXItem.setOnAction(event -> fSpMX.setKeyboardMode(false));
+        modeSTItem.setOnAction(event -> fSpMX.setKeyboardMode(true ));
+
+        // -= О программе =-
+        aboutItem.setOnAction(event -> {
+            String name      = "Эмулятор \"".concat(SPMX_NAME).concat("\"");
+            String version   = "x.x.x.x";
+            String copyright = "Copyright © 2018 \"AVSh Software\" (Александр Шевцов)";
+
+            final InputStream is = getResourceAsStream(SPMX_PROP_FILE);
+            if (is != null) {
+                try (InputStreamReader isr = new InputStreamReader(is, "UTF-8")) {
+                    final Properties property = new Properties();
+                    property.load(isr);
+                    name      = property.getProperty("productName"  , name     );
+                    version   = property.getProperty("versionNumber", version  );
+                    copyright = property.getProperty("copyright"    , copyright);
+                } catch (IOException ex) {
+                    //
+                }
+            }
+            final Alert alert = new Alert(INFORMATION);
+            alert.setTitle      ("Информация");
+            alert.setHeaderText (null);
+            alert.setContentText(String.format("%s v%s%n%n%s", name, version, copyright));
+            alert.showAndWait   ();
+        });
 
         // -= Обработка событий клавиатуры (нажатие) =-
         scene.setOnKeyPressed(event -> {
