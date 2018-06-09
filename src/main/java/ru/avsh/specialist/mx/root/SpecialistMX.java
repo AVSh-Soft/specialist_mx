b53d9c9f3d339ea61939d718e0c32a91659c3117
Alexander Shevtsov <avsh-soft@rambler.ru>
2018-06-09 09:13:13 +0300

    Переход на fx (переделка главного окна на JavaFX)

--- src/main/java/ru/avsh/specialist/mx/root/SpecialistMX.java
+++ src/main/java/ru/avsh/specialist/mx/root/SpecialistMX.java
@@ -112,46 +112,6 @@ public final class SpecialistMX {
         // Инициализируем переменную под имя текущего MON-файла
         fCurMonName = "";
 
-        // Русифицируем JFileChooser
-/*
-        UIManager.put("FileChooser.openButtonText"         , "Открыть"  );
-        UIManager.put("FileChooser.saveButtonText"         , "Сохранить");
-        UIManager.put("FileChooser.cancelButtonText"       , "Отменить" );
-        UIManager.put("FileChooser.directoryOpenButtonText", "Открыть"  );
-
-        UIManager.put("FileChooser.lookInLabelText"     , "Смотреть в:" );
-        UIManager.put("FileChooser.saveInLabelText"     , "Сохранить в:");
-        UIManager.put("FileChooser.fileNameLabelText"   , "Имя файла:"  );
-        UIManager.put("FileChooser.filesOfTypeLabelText", "Тип файла:"  );
-
-        UIManager.put("FileChooser.openButtonToolTipText"  , "Открыть выделенный файл"     );
-        UIManager.put("FileChooser.saveButtonToolTipText"  , "Сохранить выбранный файл"    );
-        UIManager.put("FileChooser.cancelButtonToolTipText", "Прервать диалог выбора файла");
-
-        UIManager.put("FileChooser.listViewButtonToolTipText"     , "Список"       );
-        UIManager.put("FileChooser.detailsViewButtonToolTipText"  , "Детально"     );
-        UIManager.put("FileChooser.directoryOpenButtonToolTipText", "Открыть папку");
-
-        UIManager.put("FileChooser.upFolderToolTipText"  , "На уровень выше");
-        UIManager.put("FileChooser.homeFolderToolTipText", "В начало"       );
-        UIManager.put("FileChooser.newFolderToolTipText" , "Создать папку"  );
-
-        UIManager.put("FileChooser.fileNameHeaderText", "Имя"     );
-        UIManager.put("FileChooser.fileSizeHeaderText", "Размер"  );
-        UIManager.put("FileChooser.fileTypeHeaderText", "Тип"     );
-        UIManager.put("FileChooser.fileDateHeaderText", "Изменен" );
-        UIManager.put("FileChooser.fileAttrHeaderText", "Атрибуты");
-
-        UIManager.put("FileChooser.acceptAllFileFilterText", "Все файлы");
-*/
-
-        // Русифицируем JOptionPane
-/*
-        UIManager.put("OptionPane.yesButtonText"   , "Да"      );
-        UIManager.put("OptionPane.noButtonText"    , "Нет"     );
-        UIManager.put("OptionPane.cancelButtonText", "Отменить");
-*/
-
         // Запускаем тактовый генератор
         new Thread(fGen).start();
         // Запускаем эмулятор
