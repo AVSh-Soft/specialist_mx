a6ea5d6666a81ffd0e30fa10878ac9e456e9dad8
AVSh <avsh-soft@rambler.ru>
2019-08-06 11:37:02 +0300

    Попытка перейти на OpenJDK

--- src/main/java/ru/avsh/specialist/mx/gui/utils/PixelatedImageView.java
+++ src/main/java/ru/avsh/specialist/mx/gui/utils/PixelatedImageView.java
@@ -14,77 +14,71 @@ import javafx.scene.image.ImageView;
 import org.apache.commons.lang3.reflect.FieldUtils;
 import org.apache.commons.lang3.reflect.MethodUtils;
 
-import java.lang.reflect.InvocationTargetException;
-
 @SuppressWarnings("restriction")
 public class PixelatedImageView extends ImageView {
-    /**
-     * Allocates a new ImageView object using the given image.
-     *
-     * @param image Image that this PixelatedImageView uses
-     */
+
     public PixelatedImageView(javafx.scene.image.Image image) {
         super(image);
+        initialize();
+    }
 
+    private void initialize() {
         try {
-            initialize();
+            final Object nodeHelper = FieldUtils.readField(this, "nodeHelper", true);
+            FieldUtils.writeField(nodeHelper, "imageViewAccessor", null, true);
+
+            ImageViewHelper.setImageViewAccessor(new ImageViewHelper.ImageViewAccessor() {
+                @Override
+                public NGNode doCreatePeer(Node node) {
+                    return new NGImageView() {
+                        private Image image;
+
+                        @Override
+                        public void setImage(Object img) {
+                            super.setImage(img);
+                            image = (Image) img;
+                        }
+
+                        @Override
+                        protected void renderContent(Graphics g) {
+                            final BaseResourceFactory factory = (BaseResourceFactory) g.getResourceFactory();
+                            Texture tex = factory.getCachedTexture(image, Texture.WrapMode.CLAMP_TO_EDGE);
+                            tex.setLinearFiltering(false);
+                            tex.unlock();
+                            super.renderContent(g);
+                        }
+                    };
+                }
+
+                @Override
+                public void doUpdatePeer(Node node) {
+                    try {
+                        MethodUtils.invokeMethod(node, true, "doUpdatePeer");
+                    } catch (ReflectiveOperationException e) {
+                        //
+                    }
+                }
+
+                @Override
+                public BaseBounds doComputeGeomBounds(Node node, BaseBounds bounds, BaseTransform tx) {
+                    try {
+                        return (BaseBounds) MethodUtils.invokeMethod(node, true, "doComputeGeomBounds", bounds, tx);
+                    } catch (ReflectiveOperationException e) {
+                        return null;
+                    }
+                }
+
+                @Override
+                public boolean doComputeContains(Node node, double localX, double localY) {
+                    try {
+                        return (boolean) MethodUtils.invokeMethod(node, true, "doComputeContains", localX, localY);
+                    } catch (ReflectiveOperationException e) {
+                        return false;
+                    }
+                }
+            });
         } catch (IllegalAccessException e) {
             //
         }
-    }
-
-    private void initialize() throws IllegalAccessException {
-        final Object nodeHelper = FieldUtils.readField(this, "nodeHelper", true);
-        FieldUtils.writeField(nodeHelper, "imageViewAccessor", null, true);
-        ImageViewHelper.setImageViewAccessor(new ImageViewHelper.ImageViewAccessor() {
-            @Override
-            public NGNode doCreatePeer(Node node) {
-                return new NGImageView() {
-                    private Image image;
-
-                    @Override
-                    public void setImage(Object img) {
-                        super.setImage(img);
-                        image = (Image) img;
-                    }
-
-                    @Override
-                    protected void renderContent(Graphics g) {
-                        BaseResourceFactory factory = (BaseResourceFactory) g.getResourceFactory();
-                        Texture tex = factory.getCachedTexture(image, Texture.WrapMode.CLAMP_TO_EDGE);
-                        tex.setLinearFiltering(false);
-                        tex.unlock();
-                        super.renderContent(g);
-                    }
-                };
-            }
-
-            @Override
-            public void doUpdatePeer(Node node) {
-                try {
-                    MethodUtils.invokeMethod(node, true, "doUpdatePeer");
-                } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
-                    //
-                }
-            }
-
-            @Override
-            public BaseBounds doComputeGeomBounds(Node node, BaseBounds bounds, BaseTransform tx) {
-                try {
-                    return (BaseBounds) MethodUtils.invokeMethod(node, true, "doComputeGeomBounds", bounds, tx);
-                } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
-                    return null;
-                }
-            }
-
-            @Override
-            public boolean doComputeContains(Node node, double localX, double localY) {
-                try {
-                    return (boolean) MethodUtils.invokeMethod(node, true, "doComputeContains", localX, localY);
-                } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
-                    return false;
-                }
-            }
-        });
     }
 }
